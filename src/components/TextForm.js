5231f7a99887dc74d967204c5a9a6b7e45f58fa3
arjunkumar <arjun.kumar@krishtechnolabs.com>
2024-04-22 20:18:11 +0530

    video 8 enhance text utils functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,11 +12,17 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleLowClick = (event) => {
+        let lowerText = text.toLowerCase();
+        setText(lowerText);
+    }
+
 // Declare a new state variable, which we'll call "count"
-const [text, setText] = useState('Enter text here');
+const [text, setText] = useState('');
     // setText('new text');
   return (
-    <div>
+    <>
+      <div className="container">
         <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea
@@ -27,7 +33,16 @@ const [text, setText] = useState('Enter text here');
           onChange={handleOnChange}
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
     </div>
+    <div className="container my-3">
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} words {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes need to read the above texts</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   );
 }
