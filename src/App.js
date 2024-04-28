12952d3792097a92cb617a0f8b6f1a83048fb2c9
arjunkumar <arjun.kumar@krishtechnolabs.com>
2024-04-28 17:21:12 +0530

    video 12 improving dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [darkMode, setDarkMode] = useState('light'); // Whether dark mode is enabled or not.
+
+  const toggleMode = () => {
+    if (darkMode === 'light') {
+      setDarkMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    } else {
+      setDarkMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About us" />
+      <Navbar title="TextUtils" aboutText="About us" mode={darkMode} toggleMode={toggleMode} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
-        <About />
+        <TextForm heading="Enter the text to analyze below" textColor={darkMode} />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,7 +40,7 @@ const [text, setText] = useState('');
     // setText('new text');
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color: props.textColor === 'dark' ? 'white' : 'black'}}>
         <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea
@@ -49,6 +49,7 @@ const [text, setText] = useState('');
           rows="8"
           value={text}
           onChange={handleOnChange}
+          style={{backgroundColor: props.textColor === 'dark' ? 'grey' : 'white', color: props.textColor === 'dark' ? 'white' : 'black'}}
         ></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
@@ -57,12 +58,12 @@ const [text, setText] = useState('');
       <button className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy Text</button>
       <button className="btn btn-primary mx-2" onClick={handleExtraSpacesClick}>Remove Extra Spaces</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.textColor === 'dark' ? 'white' : 'black'}}>
       <h2>Your text summary</h2>
       <p>{text.split(" ").length} words {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes need to read the above texts</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length > 0 ? text : "Enter something is the text box above to preview it here."}</p>
     </div>
     </>
   );
